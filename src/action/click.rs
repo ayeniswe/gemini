bd2775e802b597e66d0f1325344dde2760b299c7
Samson <nazaritee@proton.me>
2025-05-26 09:25:25 -0500

    ref: click better button api

--- src/action/click.rs
+++ src/action/click.rs
@@ -1,16 +1,36 @@
-use std::{collections::HashMap, rc::Rc};
-
-use dyn_clone::{clone_trait_object, DynClone};
 use log::debug;
-use winit::{
-    event::{Event, MouseButton, WindowEvent},
-};
+use std::{collections::HashMap, rc::Rc};
+use winit::event::{ElementState, Event, WindowEvent};
 
 use crate::ui::{
     sync::{Signal, Trigger},
     widget::BaseWidget,
 };
 
+use super::ActionHandler;
+
+/// The `MouseButton` struct are different
+/// types of mouse buttons and button states
+///
+/// The `Release` variant represents
+/// the state of a button being released
+/// the default variant is the button
+/// being pressed
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum MouseButton {
+    LeftButtonRelease,
+    LeftButton,
+    RightButtonRelease,
+    RightButton,
+    MiddleButtonRelease,
+    MiddleButton,
+    ForwardButtonRelease,
+    ForwardButton,
+    BackButtonRelease,
+    BackButton,
+    OtherButtonReleased(u16),
+    OtherButton(u16),
+}
 /// The `Click` struct allows widgets to have the ability
 /// to respond to any mouse click event
 #[derive(Clone)]
@@ -48,7 +68,7 @@ impl<State> Click<State> {
         self
     }
 }
-impl<State: Clone> ClickHandler for Click<State> {
+impl<State: Clone> ActionHandler for Click<State> {
     fn apply(&mut self, trigger: Rc<Trigger>, widget: &mut BaseWidget, e: Event<Signal>) {
         match e {
             Event::WindowEvent { ref event, .. } => match event {
@@ -56,10 +76,49 @@ impl<State: Clone> ClickHandler for Click<State> {
                     widget.state.hovered = widget.layout.is_inbounds(position.x, position.y);
                 }
                 WindowEvent::MouseInput { button, state, .. } => {
-                    if widget.state.hovered && state.is_pressed() {
-                        debug!("triggered {:?} button for widget: {}", button, widget.id);
+                    let button = match (button, state) {
+                        (winit::event::MouseButton::Left, ElementState::Pressed) => {
+                            MouseButton::LeftButton
+                        }
+                        (winit::event::MouseButton::Left, ElementState::Released) => {
+                            MouseButton::LeftButtonRelease
+                        }
+                        (winit::event::MouseButton::Right, ElementState::Pressed) => {
+                            MouseButton::RightButton
+                        }
+                        (winit::event::MouseButton::Right, ElementState::Released) => {
+                            MouseButton::RightButtonRelease
+                        }
+                        (winit::event::MouseButton::Middle, ElementState::Pressed) => {
+                            MouseButton::MiddleButton
+                        }
+                        (winit::event::MouseButton::Middle, ElementState::Released) => {
+                            MouseButton::MiddleButtonRelease
+                        }
+                        (winit::event::MouseButton::Back, ElementState::Pressed) => {
+                            MouseButton::BackButton
+                        }
+                        (winit::event::MouseButton::Back, ElementState::Released) => {
+                            MouseButton::BackButtonRelease
+                        }
+                        (winit::event::MouseButton::Forward, ElementState::Pressed) => {
+                            MouseButton::ForwardButton
+                        }
+                        (winit::event::MouseButton::Forward, ElementState::Released) => {
+                            MouseButton::ForwardButtonRelease
+                        }
+                        (winit::event::MouseButton::Other(v), ElementState::Pressed) => {
+                            MouseButton::OtherButton(*v)
+                        }
+                        (winit::event::MouseButton::Other(v), ElementState::Released) => {
+                            MouseButton::OtherButtonReleased(*v)
+                        }
+                    };
+
+                    if widget.state.hovered {
                         let handler = self.button_map.get(&button);
                         if let Some(handler) = handler {
+                            debug!("triggered {:?} for widget: {}", button, widget.id);
                             handler(&mut self.state, trigger, widget, e)
                         }
                     }
@@ -70,12 +129,3 @@ impl<State: Clone> ClickHandler for Click<State> {
         }
     }
 }
-
-/// The trait `ClickHandler` provides a
-/// way for ergonomic use for
-/// users to specify per button type
-/// click actions
-pub trait ClickHandler: DynClone {
-    fn apply(&mut self, trigger: Rc<Trigger>, widget: &mut BaseWidget, e: Event<Signal>);
-}
-clone_trait_object!(ClickHandler);
